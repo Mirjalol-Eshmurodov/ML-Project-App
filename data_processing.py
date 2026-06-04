7c4a1bfed8c65b550edae797dbce3e08d5a59438
Mirjalol-Eshmurodov <mirjaloleshmurodov1017@gmail.com>
2026-06-04 10:15:38 +0200

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -30,16 +30,20 @@ def handle_missing_values(df, strategy_dict):
     df_copy = df.copy()
     for col, strategy in strategy_dict.items():
         if strategy == "Mean":
-    imp = SimpleImputer(strategy='mean')
-    df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
-elif strategy == "Median":
-    imp = SimpleImputer(strategy='median')
-    df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
-elif strategy == "Mode":
-    imp = SimpleImputer(strategy='most_frequent')
-    df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
+            imp = SimpleImputer(strategy='mean')
+            df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
+        elif strategy == "Median":
+            imp = SimpleImputer(strategy='median')
+            df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
+        elif strategy == "Mode":
+            imp = SimpleImputer(strategy='most_frequent')
+            df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
+        elif strategy == "Drop":
+            df_copy = df_copy.dropna(subset=[col])
+        elif isinstance(strategy, (int, float, str)):
+            imp = SimpleImputer(strategy='constant', fill_value=strategy)
+            df_copy[col] = imp.fit_transform(df_copy[[col]]).flatten()
     return df_copy
-
 # smart detection of task type
 
 def detect_task_type(target_data):
